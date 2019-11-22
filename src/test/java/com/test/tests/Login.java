eaf06f5f5dc7e1084f2110be191578d69eebfd1a
Noor Yeaser Khan <yeaser.khan@sekai-lab-bd.com>
2019-11-22 14:30:18 +0600

    Added read from csv

--- src/test/java/com/test/tests/Login.java
+++ src/test/java/com/test/tests/Login.java
@@ -3,12 +3,15 @@ package com.test.tests;
 import org.testng.annotations.Test;
 import org.testng.AssertJUnit;
 
-
+import java.io.FileReader;
+import java.io.IOException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 import com.test.pages.Loginpage;
 import com.test.pages.dashboardpage;
 import com.test.pages.homepage;
@@ -21,6 +24,9 @@ public class Login extends BrowserFactory{
 	Loginpage loginpage;
 	dashboardpage dashboard;
 	
+	//String CSV_file = "/Users/yeaser.khan/Desktop/creds_fg.csv";
+	String CSV_file=System.getProperty("user.dir")+"/Resources/creds_fg.csv";
+	
 	@Test(priority=0)	
 	public void clickLoginHome() throws InterruptedException{
 		homepage = new homepage(driver);
@@ -31,17 +37,36 @@ public class Login extends BrowserFactory{
 	    }
 	
 	@Test(priority=1)
-	public void enterCredentials() throws InterruptedException{
+	public void enterCredentials() throws InterruptedException, CsvValidationException{
 		loginpage = new Loginpage(driver);
-		loginpage.login("saymanasrin39@gmail.com","s123456");
-		Thread.sleep(5000);
-	}
-	
-	@Test(priority=2)
-	public void signOut() throws InterruptedException{
-		dashboard = new dashboardpage(driver);
-		dashboard.logoutlink();
-	    Thread.sleep(3000);
+		CSVReader reader = null;
+		
+		try {
+			reader = new CSVReader(new FileReader(CSV_file));
+			String[] cell;
+			
+			while((cell=reader.readNext())!=null) {
+				String email = cell[0];
+				String pass = cell[1];
+				loginpage.login(email,pass);
+				Thread.sleep(5000);
+				dashboard = new dashboardpage(driver);
+				String curUrl=driver.getCurrentUrl();
+				System.out.println("Current URL is: " +curUrl);
+				String expUrl="https://dev.find-garage.com/dashboard";
+				System.out.println("Expected URL is: " +expUrl);
+				if(curUrl.equals(expUrl)) {
+					dashboard.logoutlink();
+				    Thread.sleep(3000);
+				}
+				else {
+					System.out.println("Incorrect Credentials Given!");
+				}
+			}
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
